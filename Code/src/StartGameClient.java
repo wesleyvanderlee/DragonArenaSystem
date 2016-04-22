603141a1890f0238d5ab91da674c873593fb97e3
lester2684 <lester2684@gmail.com>
2016-04-22 11:34:20 +0200

    update

--- Code/src/StartGameClient.java
+++ Code/src/StartGameClient.java
@@ -7,9 +7,9 @@ import RMI.GameServer;
 public class StartGameClient {
 	public static void main(String[] args) throws Exception {
 		
-		for(int k = 0; k<5; k++){
+		for(int k = 0; k<1; k++){
 			int i = 1;
-			GameClient gc = new GameClient(0, Configuration.SERVER_IDS[i], Configuration.SERVER_HOSTS[i],Configuration.SERVER_REGISTRY_PORTS[i]);
+			GameClient gc = new GameClient(45, Configuration.SERVER_IDS[i], Configuration.SERVER_HOSTS[i],Configuration.SERVER_REGISTRY_PORTS[i]);
 			Thread t  = new Thread(gc);
 			t.start();
 		}
